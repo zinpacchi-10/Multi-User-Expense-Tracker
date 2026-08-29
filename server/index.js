7d2e2fd9f36b266fb4e76defa17ac9306804382b
zinpacchi-10 <mohammadalamin2352210@gmai.com>
2026-08-29 09:13:49 +0600

    Fix index.js app.listen func

--- server/index.js
+++ server/index.js
@@ -4,6 +4,6 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
 });
