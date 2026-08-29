ad669187b24cc8c2960f506798ab66c93b979461
zinpacchi-10 <mohammadalamin2352210@gmai.com>
2026-08-30 01:40:50 +0600

    Fix API URL for production

--- public/js/api.js
+++ public/js/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://multi-user-expense-tracker-production.up.railway.app/api';
 
 function getToken() {
     return localStorage.getItem('token');
