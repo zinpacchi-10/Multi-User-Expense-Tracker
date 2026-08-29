32c066da947cedd4d43e6a542c01ebaec3a574fd
zinpacchi-10 <mohammadalamin2352210@gmai.com>
2026-08-30 02:31:44 +0600

    fix

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,19 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+let db;
+
+if (process.env.MYSQL_URL) {
+    db = mysql.createConnection(process.env.MYSQL_URL);
+} else {
+    db = mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT || 3306
+    });
+}
 
 db.connect((err) => {
     if (err) {
