9dbf1e6a8c6c5b202218945f5224fac29b761f68
zinpacchi-10 <mohammadalamin2352210@gmai.com>
2026-09-04 23:03:35 +0600

    Updated project

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,11 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+const isLocal = !process.env.DATABASE_URL || process.env.DATABASE_URL.includes('localhost');
+
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: process.env.DATABASE_URL?.includes('localhost') ? false : { rejectUnauthorized: false }
+    ssl: isLocal ? false : { rejectUnauthorized: false }
 });
 
 pool.query('SELECT NOW()', (err) => {
